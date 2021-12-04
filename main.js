c4c3a84e9fd7310b732232caa1af93745228519f
prettygirlsia <87704108+prettygirlsia@users.noreply.github.com>
2021-12-04 13:11:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("My_console_project");
+var a=10;
+var b=5;
+var add=a+b;
+console.log (add);
+
+var first="Saanvi";
+var second="Dhingra";
+var fullname=first.concat(second);
+console.log (fullname);
